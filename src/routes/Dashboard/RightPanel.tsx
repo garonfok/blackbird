9553148a9034be6da504e6e620f5bb865f0cf636
Garon Fok <fokgaron@gmail.com>
2023-08-05 02:08:58 -0700

    Add more sidebar

--- src/routes/Dashboard/RightPanel.tsx
+++ src/routes/Dashboard/RightPanel.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { ResizableRight } from "../../components/ResizeableRight";
+import Icon from "@mdi/react";
+import { mdiClose } from "@mdi/js";
 
 export function RightPanel() {
   const [maxWidth, setMaxWidth] = useState<number>(384);
@@ -20,7 +22,31 @@ export function RightPanel() {
 
   return (
     <ResizableRight width={256} minWidth={192} maxWidth={maxWidth}>
-      Hi
+      <div className="flex flex-col h-full gap-[14px]">
+        <span className="flex justify-between items-center py-[8px]">
+          <span className="text-fg.muted">Viewing</span>
+          <button className="text-fg.muted hover:text-fg.default transition-all">
+            <Icon path={mdiClose} size={1} />
+          </button>
+        </span>
+        <hr className="text-fg.subtle" />
+        <div className="flex flex-col gap-[14px] flex-grow"></div>
+        <hr className="text-fg.subtle" />
+        <div className="flex flex-col gap-[8px]">
+          <button className="text-left text-fg.muted hover:text-fg.default transition-all">
+            Open folder
+          </button>
+          <button className="text-left text-fg.muted hover:text-fg.default transition-all">
+            Edit data
+          </button>
+          <button className="text-left text-fg.muted hover:text-fg.default transition-all">
+            Print parts
+          </button>
+          <button className="text-left text-danger.default hover:text-danger.emphasis transition-all">
+            Delete
+          </button>
+        </div>
+      </div>
     </ResizableRight>
   );
 }
