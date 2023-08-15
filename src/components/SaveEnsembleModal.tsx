429cbc3abfd90483bd589bab6facb3ef0acccf46
Garon Fok <fokgaron@gmail.com>
2023-08-15 16:44:28 -0700

    Add save template ensemble

--- src/components/SaveEnsembleModal.tsx
+++ src/components/SaveEnsembleModal.tsx
@@ -1,7 +1,8 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { Modal } from "./Modal";
+import { Combobox } from "@headlessui/react";
 import { invoke } from "@tauri-apps/api";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Ensemble } from "../app/types";
+import { Modal } from "./Modal";
 
 export function SaveEnsembleModal(props: {
   closeModal: () => void;
@@ -25,6 +26,10 @@ export function SaveEnsembleModal(props: {
     };
   }, []);
 
+  useEffect(() => {
+    console.log("focus changed", isInputFocused);
+  }, [isInputFocused]);
+
   async function fetchCategories() {
     const ensembles = (await invoke("ensembles_get_all")) as Ensemble[];
     const categories = Array.from(
@@ -35,6 +40,7 @@ export function SaveEnsembleModal(props: {
 
   const handleClickOutside = useCallback((event: MouseEvent) => {
     if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
+      console.log("outside click event fired");
       setIsInputFocused(false);
     }
   }, []);
@@ -50,18 +56,51 @@ export function SaveEnsembleModal(props: {
     >
       <div className="flex flex-col gap-[14px]">
         <div className="flex flex-col gap-[8px]">
-          <label htmlFor="title" className="text-fg.muted">
+          <label htmlFor="name" className="text-fg.muted">
             Name
           </label>
           <input
-            id="title"
+            id="name"
             type="text"
             className="input-text"
             placeholder="Required"
             required
             onChange={(event) => setName(event.target.value)}
-            onFocus={() => setIsInputFocused(true)}
           />
+        </div>
+        <div className="flex flex-col gap-[8px]">
+          <label htmlFor="category" className="text-fg.muted">
+            Category
+          </label>
+          {/* <input
+            ref={inputRef}
+            id="category"
+            type="text"
+            className={classNames("input-text", isInputFocused && "border")}
+            onChange={(event) => setSelectedCategory(event.target.value)}
+            onFocus={() => setIsInputFocused(true)}
+          /> */}
+          <Combobox value={selectedCategory} onChange={setSelectedCategory}>
+            <Combobox.Input
+              className="input-text"
+              value={selectedCategory}
+              onChange={(event) => setSelectedCategory(event.target.value)}
+            />
+            <Combobox.Options
+              as="ul"
+              className="bg-bg.inset rounded-[4px] border border-bg.inset overflow-y-auto scrollbar-default max-h-60"
+            >
+              {categories.map((category) => (
+                <Combobox.Option
+                  key={category}
+                  value={category}
+                  className="first:rounded-t-[4px] last:rounded-b-[4px] dropdown-item cursor-pointer w-full"
+                >
+                  {category}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Combobox>
         </div>
       </div>
     </Modal>
