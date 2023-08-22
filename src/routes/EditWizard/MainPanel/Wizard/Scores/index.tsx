c98427e70ed09482fdebf7261c7ca131ea46c28e
Garon Fok <fokgaron@gmail.com>
2023-08-22 11:14:35 -0700

    Fix misisng file attribute

--- src/routes/EditWizard/MainPanel/Wizard/Scores/index.tsx
+++ src/routes/EditWizard/MainPanel/Wizard/Scores/index.tsx
@@ -1,14 +1,10 @@
-import { mdiChevronDown, mdiPlus } from "@mdi/js";
-import { useAppDispatch, useAppSelector } from "../../../../../app/hooks";
+import { mdiPlus } from "@mdi/js";
 import Icon from "@mdi/react";
-import { useState } from "react";
-import classNames from "classnames";
-import { ScoresList } from "./ScoresList";
+import { useAppDispatch, useAppSelector } from "../../../../../app/hooks";
 import { pushScore } from "../../../pieceSlice";
+import { ScoresList } from "./ScoresList";
 
 export function Scores() {
-  const [showingParts, setShowingParts] = useState(false);
-
   const piece = useAppSelector((state) => state.piece);
   const dispatch = useAppDispatch();
 
@@ -17,7 +13,8 @@ export function Scores() {
       pushScore({
         name: "Full score",
         renaming: false,
-        id: Math.max(...piece.scores.map((score) => score.id ?? 0)) + 1,
+        id: Math.max(...piece.scores.map((score) => score.id), 0) + 1,
+        file: null,
       })
     );
   }
@@ -32,28 +29,6 @@ export function Scores() {
         <Icon path={mdiPlus} size={1} />
         Add score
       </button>
-
-      {/* <hr className="text-fg.subtle" />
-      <button
-        onClick={() => setShowingParts(!showingParts)}
-        className="flex justify-between text-fg.muted hover:text-fg.default transition-all"
-      >
-        <span>Show parts</span>
-        <Icon
-          path={mdiChevronDown}
-          size={1}
-          className={classNames("transition-all", showingParts && "rotate-180")}
-        />
-      </button>
-      {showingParts && (
-        <div className="flex flex-col bg-bg.inset p-[4px] gap-[4px] rounded-[4px]">
-          {piece.parts.map((part) => (
-            <div key={part.id} className="">
-              <span className="text-fg.muted">{part.name}</span>
-            </div>
-          ))}
-        </div>
-      )} */}
     </div>
   );
 }
