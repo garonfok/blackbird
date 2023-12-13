f538e858808ad666fa2272a7e8cf855d1f54ca0b
Garon Fok <fokgaron@gmail.com>
2023-12-13 11:49:10 -0800

    Add context menu navigation

--- src/routes/EditWizard/LeftPanel/FileList/index.tsx
+++ src/routes/EditWizard/LeftPanel/FileList/index.tsx
@@ -5,12 +5,26 @@ import {
   Droppable,
 } from "@hello-pangea/dnd";
 import { useAppDispatch, useAppSelector } from "src/app/hooks";
-import { setFiles } from "../../filesSlice";
+import { deleteFile, setFiles } from "../../filesSlice";
 import { Card } from "./Card";
+import { MouseEvent, useRef, useState } from "react";
+import { ControlledMenu, MenuItem } from "@szhsin/react-menu";
+import { ByteFile } from "src/app/types";
+import { cleanPiece } from "../../pieceSlice";
+import { invoke } from "@tauri-apps/api";
 
 export function FileList() {
+  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
+  const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<{
+    file: ByteFile;
+    index: number;
+  }>();
+
   const files = useAppSelector((state) => state.files);
   const dispatch = useAppDispatch();
+
+  const menuRef = useRef<HTMLDivElement>(null);
 
   function handleDragEnd(result: DropResult) {
     const { destination, source } = result;
@@ -31,37 +45,108 @@ export function FileList() {
     dispatch(setFiles({ files: cloned }));
   }
 
+  function handleContextMenu(
+    event: MouseEvent<HTMLDivElement>,
+    file: ByteFile,
+    index: number
+  ) {
+    if (typeof document.hasFocus === "function" && !document.hasFocus()) return;
+    event.preventDefault();
+    setAnchorPoint({ x: event.clientX, y: event.clientY });
+    setIsContextMenuOpen(true);
+    setSelectedFile({ file, index });
+  }
+
+  async function handleClickOpenFile() {
+    await invoke("open", { path: selectedFile!.file.name });
+  }
+
+  function handleClickOpenContainingDirectory() {
+    const fileName = selectedFile!.file.name;
+    const directoryPath = fileName.substring(0, fileName.lastIndexOf("/"));
+    invoke("open", { path: directoryPath });
+  }
+
+  function handleClickRemove() {
+    dispatch(deleteFile(selectedFile!.index));
+    dispatch(cleanPiece(selectedFile!.file.id));
+  }
+
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <Droppable droppableId={"dropppable"} direction="vertical">
-        {(provided) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            className="flex flex-col flex-grow overflow-y-auto scrollbar-default"
+    <>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId={"dropppable"} direction="vertical">
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="flex flex-col flex-grow overflow-y-auto scrollbar-default"
+            >
+              {files.map((file, index) => (
+                <Draggable
+                  key={file.id}
+                  draggableId={file.id.toString()}
+                  index={index}
+                >
+                  {(provided) => (
+                    <div
+                      className="mb-[8px] last:mb-0"
+                      onContextMenu={(event) =>
+                        handleContextMenu(event, file, index)
+                      }
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <Card file={file} index={index} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+      <ControlledMenu
+        anchorPoint={anchorPoint}
+        state={isContextMenuOpen ? "open" : "closed"}
+        direction="right"
+        onClose={() => {
+          setIsContextMenuOpen(false);
+          setSelectedFile(undefined);
+        }}
+      >
+        <div
+          ref={menuRef}
+          className="flex flex-col w-[192px] p-[4px] rounded-[4px] absolute bg-bg.inset shadow-float"
+        >
+          {/* <MenuItem
+            onClick={() => handleClickRename()}
+            className="context-menu-item"
           >
-            {files.map((file, index) => (
-              <Draggable
-                key={file.id}
-                draggableId={file.id.toString()}
-                index={index}
-              >
-                {(provided) => (
-                  <div
-                    className="mb-[8px] last:mb-0"
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    <Card file={file} index={index} />
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+            Rename
+          </MenuItem> */}
+          <MenuItem
+            onClick={() => handleClickOpenFile()}
+            className="context-menu-item"
+          >
+            Open file
+          </MenuItem>
+          <MenuItem
+            onClick={() => handleClickOpenContainingDirectory()}
+            className="context-menu-item"
+          >
+            Open containing directory
+          </MenuItem>
+          <MenuItem
+            onClick={() => handleClickRemove()}
+            className="context-menu-item text-danger.default hover:text-danger.emphasis"
+          >
+            Remove
+          </MenuItem>
+        </div>
+      </ControlledMenu>
+    </>
   );
 }
