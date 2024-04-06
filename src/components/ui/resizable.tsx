e278bc81679b0470688c6840620312b4f59a7531
Garon Fok <fokgaron@gmail.com>
2024-04-06 13:57:23 -0700

    feat: change handle color on focus

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -26,8 +26,8 @@ const ResizableHandle = ({
     className={cn(
       "relative flex w-px items-center justify-center bg-divider.default",
       "after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2",
-      "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-divider.focus",
-      "hover:ring-1 hover:ring-divider.focus",
+      "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-divider.focus focus-visible:bg-divider.focus",
+      "hover:ring-1 hover:ring-divider.focus hover:bg-divider.focus",
       "data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
     )}
