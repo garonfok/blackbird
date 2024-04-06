d181259b2c76c64fbd4026beb7cf964763f14e84
Garon Fok <fokgaron@gmail.com>
2024-04-05 20:30:23 -0700

    feat: add separator between Navbar and Table

--- src/routes/Dashboard/MainPanel/index.tsx
+++ src/routes/Dashboard/MainPanel/index.tsx
@@ -1,11 +1,13 @@
 import { ResizablePanel } from "@/components/ui/resizable";
 import { Navbar } from "./Navbar";
 import { Table } from "./Table";
+import { Separator } from "@/components/ui/separator";
 
 export function MainPanel() {
   return (
     <ResizablePanel className="w-full flex flex-col bg-main-bg.default">
       <Navbar />
+      <Separator />
       <Table />
     </ResizablePanel>
   );
