4787f646ebe5a329c77c5eb9f4eed6de7fcb2092
Garon Fok <fokgaron@gmail.com>
2024-04-12 22:46:21 -0700

    refactor: remove unused components and dependencies

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,21 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import filesReducer from "../routes/EditWizard/filesSlice";
-import pieceReducer from "../routes/EditWizard/pieceSlice";
-import musiciansReducer from "../routes/EditWizard/MainPanel/Wizard/musiciansSlice";
 import queryReducer from "../routes/Dashboard/MainPanel/querySlice";
-import previewReducer from "../routes/Dashboard/reducers/previewSlice";
 import filterReducer from "../routes/Dashboard/reducers/filterSlice";
-import tagsReducer from "../routes/Dashboard/reducers/tagsSlice";
 import piecesReducer from "../routes/Dashboard/reducers/piecesSlice";
+import previewReducer from "../routes/Dashboard/reducers/previewSlice";
 import setlistReducer from "../routes/Dashboard/reducers/setlistSlice";
 import setlistsReducer from "../routes/Dashboard/reducers/setlistsSlice";
-import loadingReducer from "../routes/EditWizard/loadingSlice";
+import tagsReducer from "../routes/Dashboard/reducers/tagsSlice";
 
 const store = configureStore({
   reducer: {
-    files: filesReducer,
-    piece: pieceReducer,
-    musicians: musiciansReducer,
     query: queryReducer,
     preview: previewReducer,
     filter: filterReducer,
@@ -23,7 +16,6 @@ const store = configureStore({
     pieces: piecesReducer,
     setlist: setlistReducer,
     setlists: setlistsReducer,
-    loading: loadingReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
