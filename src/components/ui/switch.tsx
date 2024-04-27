c078ef90d591d7d89f2df50eb0593549676e63c5
Garon Fok <fokgaron@gmail.com>
2024-04-26 18:33:43 -0700

    feat: reduce focus outline

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -9,7 +9,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-fg.0 focus-visible:ring-offset-2 focus-visible:ring-offset-fg.0 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary.default data-[state=unchecked]:bg-bg.2",
+      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border border-transparent transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-fg.0 focus-visible:ring-offset-1 focus-visible:ring-offset-fg.0 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary.default data-[state=unchecked]:bg-bg.2",
       className
     )}
     {...props}
