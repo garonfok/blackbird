bc9451469fbbf6ee1f80ff009fdeebee3c862e7f
Garon Fok <fokgaron@gmail.com>
2024-06-01 16:51:31 -0700

    refactor: Remove unused packages and functions

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -10,10 +10,6 @@ pub trait ServiceAccess {
     fn db<F, TResult>(&self, operation: F) -> TResult
     where
         F: FnOnce(&DatabaseConnection) -> TResult;
-
-    fn db_mut<F, TResult>(&self, operation: F) -> TResult
-    where
-        F: FnOnce(&mut DatabaseConnection) -> TResult;
 }
 
 impl ServiceAccess for AppHandle {
@@ -24,17 +20,6 @@ impl ServiceAccess for AppHandle {
         let app_state: State<AppState> = self.state();
         let db_connection_guard = app_state.db.lock().unwrap();
         let db = db_connection_guard.as_ref().unwrap();
-
-        operation(db)
-    }
-
-    fn db_mut<F, TResult>(&self, operation: F) -> TResult
-    where
-        F: FnOnce(&mut DatabaseConnection) -> TResult,
-    {
-        let app_state: State<AppState> = self.state();
-        let mut db_connection_guard = app_state.db.lock().unwrap();
-        let db = db_connection_guard.as_mut().unwrap();
 
         operation(db)
     }
