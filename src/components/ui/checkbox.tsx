8e829fc61403596feefffe4c53c6776bc769c6a0
Garon Fok <fokgaron@gmail.com>
2024-06-23 17:15:49 -0700

    fix: Add correct styling for indeterminate checkbox

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -2,7 +2,7 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import * as React from "react"
 
 import { cn } from "@/app/utils"
-import { mdiCheckBold } from "@mdi/js"
+import { mdiCheckBold, mdiMinusThick } from "@mdi/js"
 import Icon from "@mdi/react"
 
 const Checkbox = React.forwardRef<
@@ -12,7 +12,11 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-default border border-fg.2 float-shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-fg.0 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-fg.0 data-[state=checked]:text-bg.0",
+      "group peer h-4 w-4 shrink-0 rounded-default border border-fg.2 float-shadow",
+      "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-fg.0",
+      "disabled:cursor-not-allowed disabled:opacity-50",
+      "data-[state=checked]:bg-fg.0 data-[state=checked]:text-bg.0",
+      "data-[state=indeterminate]:bg-fg.0 data-[state=indeterminate]:text-bg.0",
       className
     )}
     {...props}
@@ -20,7 +24,8 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Indicator
       className={cn("relative flex items-center justify-center text-current")}
     >
-      <Icon path={mdiCheckBold} size={1 / 2} className="shrink-0 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+      <Icon path={mdiCheckBold} size={1 / 2} className="shrink-0 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-data-[state=checked]:block" />
+      <Icon path={mdiMinusThick} size={1 / 2} className="shrink-0 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-data-[state=indeterminate]:block" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
